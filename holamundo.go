46216a0eb98fbe641ecd0fe416302fc2f560565d
lharo <haroomg@gmail.com>
2025-03-31 15:59:55 -0400

    first commit

---
+++ holamundo.go
@@ -0,0 +1,51 @@
+package main
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main()  {
+
+	//// strings
+	// var str string
+
+	//// booleans
+	// var b bool
+
+	//// ints 
+	// // numeros enteros
+	// var entero8 int8
+	// var entero16 int16
+	// var entero32 int32
+	// var entero64 int64
+
+	// // numeros enteros sin signos
+	// var uentero8 uint8
+	// var uentero16 uint16
+	// var uentero32 uint32
+	// var uentero64 uint64
+
+	// // alias 
+	// var byte byte // alias de uint8
+	// var rune rune // alias de int32
+
+	// // tipos dependientes de la plataforma
+	// var enteroUint uint // 32 o 64
+	// var enteroInt int   // 32 o 64
+	// var enteroUintptr uintptr // sirve para almacenar el valor de un puntero
+
+	// //// floats
+	// // valor decimal
+	// var flotante32 float32
+	// var flotante64 float64
+
+	//// arrays
+	// var arrayint [5]int
+	// var arrayFloat [5]float
+	// var arrayBolean [5]boolean
+	// var arraystring [5]string
+
+}
+
+// link vide
+// https://www.youtube.com/watch?v=BuPK0e0i360&list=PLl_hIu4u7P64MEJpR3eVwQ1l_FtJq4a5g&index=12
